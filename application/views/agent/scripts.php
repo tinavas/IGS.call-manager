b0423ca9aea2f9751e3d4b31a60edfb90cd3c6df
Lamberto Guevarra <seoblogger07@gmail.com>
2012-07-26 10:08:51 +0800

    rename agent scripts folder

--- application/views/agent/scripts.php
+++ application/views/agent/scripts.php
@@ -13,16 +13,16 @@
         		<div class="frm_container">
         			<div class="frm_heading"><span>List of scripts</span></div>
         			<ul>
-        				<li><a href="<?php echo base_url();?>agent_scripts/COMED and NICOR DUAL TPV - Blank.htm" target="_blank">COMED and NICOR DUAL TPV - Blank</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/COMED ELECTRICITY TPV - Blank.htm" target="_blank">COMED ELECTRICITY TPV - Blank</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/FCON-499-0613MV - CROSSFIRE.htm" target="_blank">FCON-499-0613MV - CROSSFIRE</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/FCON-499-0613MV - PLAT.htm" target="_blank">FCON-499-0613MV - PLAT</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/FMC-449-0613MV - CROSSFIRE.htm" target="_blank">FMC-449-0613MV - CROSSFIRE</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/FMC-499-0613-MV - PLAT.htm" target="_blank">FMC-499-0613-MV - PLAT</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/FPECO-889-I12MV-RES-Crossfire.htm" target="_blank">FPECO-889-I12MV-RES-Crossfire</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/MICHIGAN D2D PLATINUM GAS ONLY TPV SCRIPT.htm" target="_blank">MICHIGAN D2D PLATINUM GAS ONLY TPV SCRIPT</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/NICOR GAS TPV - Blank.htm" target="_blank">NICOR GAS TPV - Blank</a></li>
-        				<li><a href="<?php echo base_url();?>agent_scripts/OHIO D2D PLATINUM DUAL ACCOUNT TPV SCRIPT.htm" target="_blank">OHIO D2D PLATINUM DUAL ACCOUNT TPV SCRIPT</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/COMED and NICOR DUAL TPV - Blank.htm" target="_blank">COMED and NICOR DUAL TPV - Blank</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/COMED ELECTRICITY TPV - Blank.htm" target="_blank">COMED ELECTRICITY TPV - Blank</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/FCON-499-0613MV - CROSSFIRE.htm" target="_blank">FCON-499-0613MV - CROSSFIRE</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/FCON-499-0613MV - PLAT.htm" target="_blank">FCON-499-0613MV - PLAT</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/FMC-449-0613MV - CROSSFIRE.htm" target="_blank">FMC-449-0613MV - CROSSFIRE</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/FMC-499-0613-MV - PLAT.htm" target="_blank">FMC-499-0613-MV - PLAT</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/FPECO-889-I12MV-RES-Crossfire.htm" target="_blank">FPECO-889-I12MV-RES-Crossfire</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/MICHIGAN D2D PLATINUM GAS ONLY TPV SCRIPT.htm" target="_blank">MICHIGAN D2D PLATINUM GAS ONLY TPV SCRIPT</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/NICOR GAS TPV - Blank.htm" target="_blank">NICOR GAS TPV - Blank</a></li>
+        				<li><a href="<?php echo base_url();?>agent-scripts/OHIO D2D PLATINUM DUAL ACCOUNT TPV SCRIPT.htm" target="_blank">OHIO D2D PLATINUM DUAL ACCOUNT TPV SCRIPT</a></li>
         			</ul>
         		</div>
 	        </div>
