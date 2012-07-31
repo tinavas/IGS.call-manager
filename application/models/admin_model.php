0ac677280819ea48e92672a827d36a776ba27f2f
Lamberto Guevarra <seoblogger07@gmail.com>
2012-07-31 18:38:00 +0800

    include sub-disposition field on report

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -8,7 +8,7 @@ class Admin_model extends CI_Model {
 	public function report($sdate, $edate, $type) {
 		if($type == 1) {
 			$query = $this->db->query("
-				SELECT a.user_name, a.agent_id, a.phone, a.account_no, a.account_no_gas, a.account_no_elec, a.customer_name, a.conf_1 as 'confirmation_no_1', a.conf_2 as 'confirmation_no_2', a.tpv_no, a.tpv_no2, a.call_record_id, a.call_record_id2, a.call_record_id3, a.promo_code, b.`label` as 'disposition', c.`label` as 'flag_reason', a.flag_others as 'flag_other_reason', d.label as 'market', a.call_notes, a.rdate as 'date' 
+				SELECT a.user_name, a.agent_id, a.phone, a.account_no, a.account_no_gas, a.account_no_elec, a.customer_name, a.conf_1 as 'confirmation_no_1', a.conf_2 as 'confirmation_no_2', a.tpv_no, a.tpv_no2, a.call_record_id, a.call_record_id2, a.call_record_id3, a.promo_code, b.`label` as 'disposition', e.`label` as 'sub-disposition', c.`label` as 'flag_reason', a.flag_others as 'flag_other_reason', d.label as 'market', a.call_notes, a.rdate as 'date' 
 				FROM igs_records a 
 				LEFT JOIN igs_dispositions b 
 				  ON a.disposition_id = b.disposition_id 
@@ -16,6 +16,8 @@ class Admin_model extends CI_Model {
 				  ON a.flag_id = c.flag_id 
 				LEFT JOIN igs_markets d 
 				  ON a.market_id = d.market_id
+				LEFT JOIN igs_dispositions e 
+				  ON a.sub_disposition_id = e.disposition_id
 				INNER JOIN (select max(record_id) as record_id from igs_records where rdate >= '{$sdate}' AND rdate <= '{$edate}' group by phone) e
 				  ON a.record_id = e.record_id 
 			");
